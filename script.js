b2759bccd42a79ba4afc060cd7b5eb170707b417
aavesh399 <85877405+aavesh399@users.noreply.github.com>
2022-09-27 10:28:10 +0530

    Currently working on it

---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementById('noti').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti2').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti3').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti4').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti5').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti6').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti7').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti8').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti9').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti10').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti11').onclick = function() {
+    alert("You have to login first");
+    
+}
+
+document.getElementById('noti12').onclick = function() {
+    alert("You have to login first");
+    
+}
